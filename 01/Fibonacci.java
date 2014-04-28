a7a1eb76427073c1905b52b93f970c829043641a
Jack5496 <mllerpeter68@yahoo.de>
2014-04-28 09:28:44 +0200

    kleine Kommentare

--- 01/Fibonacci.java
+++ 01/Fibonacci.java
@@ -18,20 +18,20 @@ public class Fibonacci implements FibonacciInterface{
 
   public int next(){
    n++;
-   return(fib(n-1));
+   return(fib(n-1));  //Aufruf der Hauptfunktion
    }
 
   private int fib( int n){
-    if(n <= 1){
-      return n;
+    if(n <= 1){ 
+      return n; //rekursions Anker
       }
     else{
-      if(n > 46){
+      if(n > 46){   //Ausnahme bei großen Werten
       System.out.println("Maximaler Integer Wert überschritten!\nErgebnis über:");
       return 2147483647;
       }
-      else{
-        return fib(n-1) + fib(n-2);
+      else{   //Rekursion
+        return fib(n-1) + fib(n-2);   //Fib Formel
       }
 
     }
