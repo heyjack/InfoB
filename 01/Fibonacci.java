5df3ba0a63a83907bc344c8f519d7a3f8f88c11a
Jack5496 <mllerpeter68@yahoo.de>
2014-04-28 13:17:52 +0200

    docs

--- 01/Fibonacci.java
+++ 01/Fibonacci.java
@@ -12,15 +12,23 @@ public class Fibonacci implements FibonacciInterface{
 
   int n;
 
+//Klassen Conrtuktor
   public Fibonacci(){
-    n = 0;
+    n = 0;  //Anfangswert auf 0
   }
 
+	/**
+	 * @return gibt die Fib Zahl zurrück
+	 */
   public int next(){
    n++;
    return(fib(n-1));  //Aufruf der Hauptfunktion
    }
 
+	/**
+	 * @return gibt die Nte Fib zahl zurrück
+	 * @param int n die Nte fib zahl
+	 */
   private int fib( int n){
     if(n <= 1){ 
       return n; //rekursions Anker
